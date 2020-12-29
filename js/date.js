906d1001ceea14e0d5e29c9fe3011a7637a50ade
Eimsepl <sorkdkrak@gmail.com>
2020-12-29 16:46:22 +0900

    112
    1

---
+++ js/date.js
@@ -0,0 +1,29 @@
+var initialDate = new Date(2020, 1, 1, 2); // Dec 1st 2012
+var now = Date.now();
+var difference = now - initialDate;
+var millisecondsPerDay = 24 * 60 * 60 * 1000;
+var daysSince = Math.floor(difference / millisecondsPerDay);
+
+var d = new Date();
+var arrL = []; 
+arrL[332] = '숨겨진 공허(가슴) / 유로파';
+arrL[333] = '벙커 E15 (머리) / 유로파';
+arrL[334] = '파멸 (다리) / 유로파';
+
+//1월
+arrL[335] = '파멸 (다리) / 유로파';
+arrL[336] = '파멸 (다리) / 유로파';
+arrL[337] = '파멸 (다리) / 유로파';
+arrL[338] = '파멸 (다리) / 유로파';
+arrL[339] = '파멸 (다리) / 유로파';
+arrL[340] = '파멸 (다리) / 유로파';
+
+var arrM = []; 
+arrM[332] = '벨레스 미궁(팔) / 발사 기지';
+arrM[333] = '숨겨진 공허 (가슴) / 유로파';
+arrM[334] = '벙커 E15 (머리) / 유로파';
+
+var year = d.getFullYear(); // 년도
+var month = d.getMonth() + 1;  // 월
+var date = d.getDate();  // 날짜
+var day = d.getDay();  // 요일
