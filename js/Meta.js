e467675e331c2386f75a7c8d425bf1277c0c97ea
destinytoday <sorkdkrak@naver.com>
2021-01-03 04:27:08 +0900

    11

--- js/Meta.js
+++ js/Meta.js
@@ -13,17 +13,17 @@ const pve_metaItemsImg = [
 
 const pvp_metaItems = [
 	"진홍 핏자국",
-	"펠윈터의 거짓말",
+	"영혼을 갉아먹는 허기",
 	"떨어지는 단두대"
 ];
 const pvp_metaItemsImg = [
 "https://www.bungie.net/common/destiny2_content/icons/9a698cbe617a95339c6f885764e68ebc.jpg",
-"https://www.bungie.net/common/destiny2_content/icons/4153f60f3f2f53ee6f7e99fa29cb4bdd.jpg",
+"https://www.bungie.net/common/destiny2_content/icons/c4509acb76551495deac51bb29b61248.jpg",
 "https://www.bungie.net/common/destiny2_content/icons/b03a9eda1da9844793d2a82f94d5e91e.jpg"
 ];
- /************ End Item paser *******************/
+/************ End Item paser *******************/
 
-/************ Python Data *******************/	
+/************ Python Data *******************/
 var pve_meta_str = "";
 var pvp_meta_str = "";
 
@@ -35,10 +35,8 @@ const itemType = [
 
 
 for (var i = 2; i >= 0; i--) {
-	pve_meta_str = "<div class='card-main'><img src='"+pve_metaItemsImg[i]+"' width=40px height=40px> ["+itemType[i]+"] "+pve_metaItems[i]+"</div><hr/>"+pve_meta_str;
+  pve_meta_str = "<div class='card-main'><img src='" + pve_metaItemsImg[i] + "' width=40px height=40px> [" + itemType[i] + "] " + pve_metaItems[i] + "</div><hr/>" + pve_meta_str;
 }
 for (var i = 2; i >= 0; i--) {
-	pvp_meta_str = "<div class='card-main'><img src='"+pvp_metaItemsImg[i]+"' width=40px height=40px> ["+itemType[i]+"] "+pvp_metaItems[i]+"</div><hr/>"+pvp_meta_str;
+  pvp_meta_str = "<div class='card-main'><img src='" + pvp_metaItemsImg[i] + "' width=40px height=40px> [" + itemType[i] + "] " + pvp_metaItems[i] + "</div><hr/>" + pvp_meta_str;
 }
-
-
