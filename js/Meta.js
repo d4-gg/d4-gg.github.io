f6295ec27f1b8e48730771cf4c04e6f767a04c19
destinytoday <sorkdkrak@naver.com>
2021-01-03 04:49:32 +0900

    6

--- js/Meta.js
+++ js/Meta.js
@@ -2,23 +2,23 @@
 
 const pve_metaItems = [
 	"일곱 번째 세라프 카빈",
-	"이켈로스_SMG_V1.0.2",
+	"영혼을 갉아먹는 허기",
 	"떨어지는 단두대"
 ];
 const pve_metaItemsImg = [
 "https://www.bungie.net/common/destiny2_content/icons/57185dba760340586551e016770d2856.jpg",
-"https://www.bungie.net/common/destiny2_content/icons/191270eda665bad75d610611cafab198.jpg",
+"https://www.bungie.net/common/destiny2_content/icons/c4509acb76551495deac51bb29b61248.jpg",
 "https://www.bungie.net/common/destiny2_content/icons/b03a9eda1da9844793d2a82f94d5e91e.jpg"
 ];
 
 const pvp_metaItems = [
 	"진홍 핏자국",
-	"영혼을 갉아먹는 허기",
+	"펠윈터의 거짓말",
 	"떨어지는 단두대"
 ];
 const pvp_metaItemsImg = [
 "https://www.bungie.net/common/destiny2_content/icons/9a698cbe617a95339c6f885764e68ebc.jpg",
-"https://www.bungie.net/common/destiny2_content/icons/c4509acb76551495deac51bb29b61248.jpg",
+"https://www.bungie.net/common/destiny2_content/icons/4153f60f3f2f53ee6f7e99fa29cb4bdd.jpg",
 "https://www.bungie.net/common/destiny2_content/icons/b03a9eda1da9844793d2a82f94d5e91e.jpg"
 ];
 /************ End Item paser *******************/
