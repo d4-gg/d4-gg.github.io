3cadadef143d3ec08d54c4e3592055b92f1e9840
Eimsepl <sorkdkrak@gmail.com>
2021-01-04 23:08:44 +0900

    132
    123

--- dps/db.js
+++
@@ -1,8 +0,0 @@
- if(daysSince2 <= '339'){ //  1월 1주차
-	for (var i = WEEK_OF_ITEMS_1; i >= 0; i--) {
-		ever_str = "<div class='card-main'>"+itemImg[i]+" "+ itemPy[i] +"</div><hr/>"+ever_str;
-		
-	}
-	W_EXO_VALUE = 2;
-	W_NF_VALUE = 5;
-}
